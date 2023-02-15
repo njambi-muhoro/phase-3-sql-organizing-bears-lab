a49ca5f234b2d0f56a04e2060a6b862964a2f31b
+ <+>
2023-02-16 02:46:55 +0300

    Assignment Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Mr. Chocolate",20,"F","Brown","snidy", 1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Rowdy",20,"F","Brown","snidy", 1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Tabitha",20,"F","Green","snidy", 1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Sergeant Brown",20,"M","Brown","snidy", 1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Melissa",20,"F","Blue","snidy", 0);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Grinch",20,"M","Brown","snidy", 1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Wendy",20,"F","Brown","snidy", 1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES(NULL, 20,"M","Yellow","snidy", 0);
